14e934f3b8150a8c6e2178c87a40bc870832e4f0
Howard Richards <dev@conficient.com>
2019-11-05 21:35:11 +0000

    Added base class for common props

---
+++ BlazorFormLayout/BsControlBase.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorFormLayout
+{
+    public class BsControlBase<TValue> : ComponentBase
+    {
+        /// <summary>
+        /// Label for the form control
+        /// </summary>
+        [Parameter] public string Label { get; set; }
+
+        /// <summary>
+        /// `id` for the input control
+        /// </summary>
+        [Parameter] public string Id { get; set; }
+
+        /// <summary>
+        /// Edit mode (cascaded param from the BsLayout control)
+        /// </summary>
+        [CascadingParameter] public EditMode Mode {get;set;}
+
+        /// <summary>
+        /// The bound value
+        /// </summary>
+        [Parameter] public TValue Value { get; set; }
+
+        /// <summary>
+        /// Value changed callback
+        /// </summary>
+        [Parameter] public EventCallback<TValue> ValueChanged { get; set; }
+
+}
+}
---
+++ BlazorFormLayout/EnumHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorFormLayout
+{
+    /// <summary>
+    /// Helper classes
+    /// </summary>
+    public static class EnumHelper
+    {
+        /// <summary>
+        /// Get a set of key-pair values
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <returns></returns>
+        public static List<KeyValuePair<string,TEnum>> GetValueOptions<TEnum>()
+        {
+            var result = new List<KeyValuePair<string, TEnum>>();
+            var names = Enum.GetNames(typeof(TEnum)).Cast<string>().ToList();
+            var values = Enum.GetValues(typeof(TEnum)).Cast<TEnum>().ToList();
+            for (int i = 0; i < names.Count; i++)
+            {
+                result.Add(new KeyValuePair<string, TEnum>(names[i], values[i]));
+            }
+            return result;
+        }
+    }
+}
