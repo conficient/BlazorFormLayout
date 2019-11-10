5ab615526d0bc7583fe8b15e6097d88cb3b9b3b0
Howard Richards <dev@conficient.com>
2019-11-10 22:40:53 +0000

    changede enum store

--- BlazorFormLayout/Helper.cs
+++ BlazorFormLayout/Helper.cs
@@ -16,14 +16,14 @@ namespace BlazorFormLayout
         /// </summary>
         /// <typeparam name="TEnum"></typeparam>
         /// <returns></returns>
-        public static List<KeyValuePair<string,TEnum>> GetValueOptions<TEnum>()
+        public static Dictionary<TEnum, string> GetValueOptions<TEnum>()
         {
-            var result = new List<KeyValuePair<string, TEnum>>();
+            var result = new Dictionary<TEnum, string>();
             var names = Enum.GetNames(typeof(TEnum)).Cast<string>().ToList();
             var values = Enum.GetValues(typeof(TEnum)).Cast<TEnum>().ToList();
             for (int i = 0; i < names.Count; i++)
             {
-                result.Add(new KeyValuePair<string, TEnum>(names[i], values[i]));
+                result.Add(values[i], names[i]);
             }
             return result;
         }
@@ -36,4 +36,5 @@ namespace BlazorFormLayout
 
         private static int _nextNameId = 0;
     }
+
 }
